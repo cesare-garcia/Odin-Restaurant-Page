5ed700d3c3a1852e0a3daf5ef09eaf3f83bbca76
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-06-26 15:01:27 -0500

    complete initial page loading module

--- src/loadpage.js
+++ src/loadpage.js
@@ -7,28 +7,43 @@ const loadPage = (contentDiv) => {
     const topTab1 = document.createElement("div");
     topTab1.classList.add("topTab");
     topTab1.setAttribute("id","tab1")
+
+    const topTab1P = document.createElement("p");
+    topTab1P.innerText = "Home";
+
     const topTab2 = document.createElement("div");
     topTab2.classList.add("topTab");
     topTab2.setAttribute("id","tab2")
+
+    const topTab2P = document.createElement("p");
+    topTab2P.innerText = "Menu";
+
     const topTab3 = document.createElement("div");
     topTab3.classList.add("topTab");
     topTab3.setAttribute("id","tab3")
 
+    const topTab3P = document.createElement("p");
+    topTab3P.innerText = "Contact";
+
     const bodyDiv = document.createElement("div");
     bodyDiv.classList.add("bodyDiv");
 
-    const middleDiv = document.createElement("div");
-    middleDiv.classList.add("middleDiv")
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("infoDiv");
 
     const bottomDiv = document.createElement("div");
     bottomDiv.classList.add("bottomDiv")
 
     contentDiv.appendChild(topDiv);
     topDiv.appendChild(topTab1);
+    topTab1.appendChild(topTab1P)
     topDiv.appendChild(topTab2);
+    topTab2.appendChild(topTab2P)
     topDiv.appendChild(topTab3);
+    topTab3.appendChild(topTab3P)
+
     contentDiv.appendChild(bodyDiv);
-    bodyDiv.appendChild(middleDiv);
+    bodyDiv.appendChild(infoDiv);
     contentDiv.appendChild(bottomDiv);
 
 };
