e25cc1e7f915de425d26f15a7e8702a43eee7e06
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-06-26 15:02:18 -0500

    add event listeners to tabs and styling to basic and home modules

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import './style.css';
-import loadPage from './loadpage.js';
 import TheMartianDiningClub from './../img/theMartianDiningClub.png';
+import GalacticMap from './../img/GalacticMap.png';
+import loadPage from './loadpage.js';
+import loadHomeTab from './loadHomeTab.js';
+
 
 const contentDiv = document.querySelector("#content");
+const mdcURL = TheMartianDiningClub;
+
+console.log(mdcURL);
 
 loadPage(contentDiv);
+loadHomeTab(mdcURL);
 
 const tab1 = document.querySelector("#tab1");
 const tab2 = document.querySelector("#tab2");
 const tab3 = document.querySelector("#tab3");
 
 tab1.addEventListener("click", (e) => {
-    console.log("tab1")
+    loadHomeTab(mdcURL);
 })
 
 tab2.addEventListener("click", (e) => {
